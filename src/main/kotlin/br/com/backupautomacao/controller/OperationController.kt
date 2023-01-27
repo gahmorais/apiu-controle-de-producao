5da95176d2a791cc973ef9aeb729a45362e8b30f
Gabriel Morais <gabriel@backupautomacao.com.br>
2023-01-27 17:06:39 -0300

    Atualizando inserção de dados

--- src/main/kotlin/br/com/backupautomacao/controller/OperationController.kt
+++ src/main/kotlin/br/com/backupautomacao/controller/OperationController.kt
@@ -1,4 +1,15 @@
 package br.com.backupautomacao.controller
 
+import br.com.backupautomacao.db.databaseInstance
+import br.com.backupautomacao.entities.Operation
+import br.com.backupautomacao.entities.Operations
+import org.ktorm.support.postgresql.insertOrUpdate
+
 class OperationController {
+
+  fun saveOperation(operation: Operation) = databaseInstance
+    .insertOrUpdate(Operations){
+      set(it.type, operation.type)
+      set(it.size, operation.size)
+    }
 }
